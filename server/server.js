31f5786453bbe7eeea389f034629d8975aecce8f
Ryan Waszak <rwaszak@tesla.com>
2021-06-24 11:30:10 -0700

    configure mongodb connection string and apollo client

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://rwas2505:ENTERPASSWORD@cluster0.ojekz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://dbUser:ixmPnNnpGUffRxN2@cluster0.01lsq.mongodb.net/lyricaldb?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
