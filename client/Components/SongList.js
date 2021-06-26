fa18729a896978a33742bb52f628ef57b934b359
Ryan Waszak <rwaszak@tesla.com>
2021-06-25 18:42:34 -0700

    bond delete mutation to SongList component in addition to our query lecture 53

--- client/Components/SongList.js
+++ client/Components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Link } from 'react-router';
 import query from '../queries/fetchSongs';
 
@@ -33,4 +34,14 @@ class SongList extends Component {
     }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+mutation DeleteSong($id: ID){
+    deleteSong(id: $id){
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(
+    graphql(query)(SongList)
+);
