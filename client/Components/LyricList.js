bde3fd19b0359e42aee1c285c870b26fe247833a
Ryan Waszak <rwaszak@tesla.com>
2021-06-28 12:47:11 -0700

    use optimistic response to increase the response time of the number of likes display by guessing at what the response will be. If the actual response differs, the component will update to the correct response.

--- client/Components/LyricList.js
+++ client/Components/LyricList.js
@@ -4,8 +4,18 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LyricList extends Component {
-    onLike(id) {
-        this.props.mutate({variables: { id } });
+    onLike(id, likes) {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                __typename: "Mutation",
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1 
+                }
+            }
+        });
     }
 
     renderLyrics() {
@@ -14,7 +24,7 @@ class LyricList extends Component {
                 <li key={id} className="collection-item">
                     {content}
                     <div className="vote-box">
-                        <i className="material-icons" onClick={() => this.onLike(id)}>
+                        <i className="material-icons" onClick={() => this.onLike(id, likes)}>
                             thumb_up
                         </i>
                         {likes}
