026756664fdaee1d8b725f8fb5131f4f197dd1f3
Ryan Waszak <rwaszak@tesla.com>
2021-06-28 09:56:24 -0700

    create fetchSong query, also console.log props to show that the id from the route such as localhost:4000/#/songs/1 logs to the params field of the props object

--- client/Components/SongDetail.js
+++ client/Components/SongDetail.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>
                 <h3>Song Detail</h3>
